5eea45079e414279f3707e2f0882d60c5e72fe7f
Anirutjame <anirutjams@gmail.com>
2025-06-17 15:33:12 +0000

    cell03

---
+++ cell03/ex00/script.js
@@ -0,0 +1,17 @@
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Get the button element
+const changeColorBtn = document.getElementById('changeColorBtn');
+
+// Add event listener for the button click
+changeColorBtn.addEventListener('click', () => {
+  // Change the background color to a random color
+  document.body.style.backgroundColor = getRandomColor();
+});
